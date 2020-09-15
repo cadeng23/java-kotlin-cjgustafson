b080b9f7c72a5d4ee0f17d4681a61999f4fc40b4
Caden Gustafson <gustafsoncaden@gmail.com>
2020-09-14 21:33:35 -0600

    skeleton for Test

---
+++ Goshdarn/app/src/test/java/com/example/goshdarn/GoshDarnUnitTest.java
@@ -0,0 +1,22 @@
+package com.example.goshdarn;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GoshDarnUnitTest {
+
+    @Test public void getTotalCrap() {
+
+    }
+
+    @Test public void haveBillsTrue() {
+
+    }
+
+    @Test public void haveBillsFalse() {
+
+    }
+
+
+}
