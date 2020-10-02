ba917a23b8bcccb90a4164cce71c7dcde8ec79a2
Caden Gustafson <gustafsoncaden@gmail.com>
2020-10-01 20:27:14 -0600

    setting main class

--- ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
+++ ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
@@ -15,6 +15,19 @@ import android.view.MenuItem;
 // I did not want to expand on my previous project because I do not like the concept anymore
 // I decided to do this project on a corporate structure of a telemarketing company
  class Employee {
+private String worker = "Employee";
+private String level = "CEO";
+private String company = "TeleCorps";
+private Double pay = 1.50;
+public String getWorker() {
+ return worker;
+}
+protected void setLevel(String level) {
+ this.worker = worker;
+}
+}
+
+class Executive extends Employee {
 
 }
 
