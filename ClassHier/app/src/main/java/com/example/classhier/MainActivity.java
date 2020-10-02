e0c9452332b8bdad46de4bf6a700aaf15884c186
Caden Gustafson <gustafsoncaden@gmail.com>
2020-10-01 19:58:31 -0600

    skeleton

---
+++ ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.classhier;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+// I did not want to expand on my previous project because I do not like the concept anymore
+// I decided to do this project on a corporate structure of a telemarketing company
+ class Employee {
+
+}
+
+class Management extends Employee {
+
+}
+
+class callers extends Employee {
+
+}
+
+class janitors extends Employee {
+
+}
