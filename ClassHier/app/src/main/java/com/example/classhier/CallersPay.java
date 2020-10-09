c11d189a8cca6334a08997c3290373a53cf2968f
Caden Gustafson <gustafsoncaden@gmail.com>
2020-10-08 20:22:04 -0600

    Fixing up the pay and commission

---
+++ ClassHier/app/src/main/java/com/example/classhier/CallersPay.java
@@ -0,0 +1,5 @@
+package com.example.classhier;
+
+public class CallersPay {
+    callers.pay
+}
--- ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
+++ ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
@@ -4,53 +4,63 @@ package com.example.classhier;
 // I decided to do this project on a corporate structure of a telemarketing company
 
 class Employee {
-public String worker = "Employee";
-public String level = "CEO";
-public String company = "TeleCorps";
-public Double pay = 35000.0;
-public String comBased = "Yes, .1x Commission";
 
-public String getWorker() {
- return worker;
-}
- protected void setWorker(String worker) {
- this.worker = worker;
-}
-//getting the level of the company that the employee is at
+ public String level = "CEO";
+ public Double pay = 0.0;
+ public Boolean comBased = true;
+ public Double Commission = 1.0;
+ // Stock Sold on the phone over the previous month
+ public Double ValueSold = 0.0;
+ public Double HoursWorked = 40.0;
+ //Rating on service given by customers on
+ public Double SurveyRating = 3.0;
+
+ public Double getValueSold() {
+  return ValueSold;
+ }
+ protected void setValueSold(Double ValueSold) {
+  this.ValueSold = ValueSold;
+ }
+
+ //getting the level of the company that the employee is at
  protected String getLevel() {
   return level;
-}
+ }
  protected void setLevel(String level) {
   this.level = level;
-}
-// Getting the Company that these individuals work for
- protected String getCompany(){
-  return company;
-}
- protected void setCompany(String company) {
-  this.company = company;
-}
-//func for getting the base pay for each level
- protected Double getPay(){
-  return pay;
-}
+ }
+
+ // Getting the Company that these individuals work for
+
+
+ //func for getting the base pay for each level
+ protected Double getPay() { return pay; }
  protected void setPay(Double pay) {
   this.pay = pay;
-}
- void task() { System.out.println("Working");
  }
- protected String getComBased() {return comBased;}
- protected void setComBased(String comBased){this.comBased = comBased;}
+
+ void task() { System.out.println("Working"); }
+
+ protected Boolean TComBased() { return true; }
+ protected Boolean FComBased() {return false;}
+
+ public Double getCommission() { return Commission; }
+ public Double setCommission(Double Commission) { this.Commission = Commission; return Commission; }
+
+ public Double getHoursWorked() { return HoursWorked; }
+ public void setHoursWorked(Double HoursWorked;) { this.HoursWorked = HoursWorked; }
+
+ public Double getSurveyRating() { return SurveyRating; }
+ public void setSurveyRating(Double SurveyRating) { this.SurveyRating = SurveyRating; }
 }
 
 class Executive extends Employee {
  void getData1()
  {
-  getWorker();
-  getCompany();
   setPay(150000.0);
+  setHoursWorked(30.0);
   setLevel("Executive");
-  setComBased("Nope");
+  FComBased();
   task();
  }
 
@@ -59,11 +69,10 @@ class Executive extends Employee {
 class Management extends Employee {
   void getData2()
   {
-   getWorker();
-   getCompany();
    setPay(80000.0);
+   setHoursWorked(40.0);
    setLevel("Management");
-   setComBased("Based on performance of callers");
+   TComBased();
    task();
   }
 }
@@ -71,21 +80,73 @@ class Management extends Employee {
 class callers extends Employee {
   void getData3()
   {
-   getWorker();
-   getCompany();
    setPay(25000.0);
    setLevel("Caller");
-   getComBased();
+   TComBased();
+   setValueSold(50000.0);
+   Payday = setPay(25000.0);
+   setSurveyRating(3.5);
+   setHoursWorked(45.0);
+
    task();
+
+   if (Serv > 4.4)
+   {
+    double Com = setCommission(1.5);
+    NewPay = Com * Payday;
+   }
+   else if (Serv > 3.9)
+   {
+    Com = setCommission(1.3);
+    NewPay = Com * Payday;
+   }
+   else if (Serv > 2.9)
+   {
+    Com = setCommission(1.1);
+    NewPay = Com * Payday;
+   }
+   else
+   {
+    getCommission();
+    NewPay = Payday;
+   }
   }
 }
 
-class janitors extends Employee {
-  void getData4()
+class cusService extends Employee {
+ void getData4()
+ {
+  setLevel("Customer Service");
+  getComBased();
+  getHoursWorked();
+  Payday = setPay(30000.0);
+  Serv = setSurveyRating(4.5);
+  if (Serv > 4.4)
   {
-   getWorker();
-   //Janitors leased from different company
-   setCompany("Cleaners for Rent");
+   Com = setCommission(1.5);
+   NewPay = Com * Payday;
+  }
+  else if (Serv > 3.9)
+  {
+   Com = setCommission(1.3);
+   NewPay = Com * Payday;
+  }
+  else if (Serv > 2.9)
+  {
+   Com = setCommission(1.1);
+   NewPay = Com * Payday;
+  }
+  else
+  {
+   getCommission();
+   NewPay = Payday;
+  }
+ }
+}
+
+class janitors extends Employee {
+  void getData5()
+  {
    setLevel("Janitor");
    setComBased("No");
    task();
