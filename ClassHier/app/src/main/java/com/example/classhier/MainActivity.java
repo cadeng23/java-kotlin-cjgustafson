f5360a775e160bac686649780b461f4ebb94c361
Caden Gustafson <gustafsoncaden@gmail.com>
2020-10-08 20:57:49 -0600

    Finished Class Hierarchy

--- ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
+++ ClassHier/app/src/main/java/com/example/classhier/MainActivity.java
@@ -18,9 +18,7 @@ class Employee {
  public Double getValueSold() {
   return ValueSold;
  }
- protected void setValueSold(Double ValueSold) {
-  this.ValueSold = ValueSold;
- }
+ protected Double setValueSold(Double ValueSold) { this.ValueSold = ValueSold; return ValueSold; }
 
  //getting the level of the company that the employee is at
  protected String getLevel() {
@@ -30,14 +28,9 @@ class Employee {
   this.level = level;
  }
 
- // Getting the Company that these individuals work for
-
-
  //func for getting the base pay for each level
  protected Double getPay() { return pay; }
- protected void setPay(Double pay) {
-  this.pay = pay;
- }
+ protected Double setPay(Double pay) { this.pay = pay; return pay; }
 
  void task() { System.out.println("Working"); }
 
@@ -47,11 +40,12 @@ class Employee {
  public Double getCommission() { return Commission; }
  public Double setCommission(Double Commission) { this.Commission = Commission; return Commission; }
 
+ //Hours worked in the week
  public Double getHoursWorked() { return HoursWorked; }
- public void setHoursWorked(Double HoursWorked;) { this.HoursWorked = HoursWorked; }
+ public void setHoursWorked(Double HoursWorked) { this.HoursWorked = HoursWorked; }
 
  public Double getSurveyRating() { return SurveyRating; }
- public void setSurveyRating(Double SurveyRating) { this.SurveyRating = SurveyRating; }
+ public Double setSurveyRating(Double SurveyRating) { this.SurveyRating = SurveyRating; return SurveyRating;}
 }
 
 class Executive extends Employee {
@@ -67,88 +61,98 @@ class Executive extends Employee {
 }
 
 class Management extends Employee {
+ double Profit;
+ double NewPay;
   void getData2()
   {
-   setPay(80000.0);
    setHoursWorked(40.0);
    setLevel("Management");
    TComBased();
    task();
+   if (Profit > 1000000.0)
+   {
+    NewPay = setCommission(1.7) * setPay(80000.0);
+   }
+   else if(Profit > 900000.0)
+   {
+    NewPay = setCommission(1.5) * setPay(80000.0);
+   }
+   else if(Profit > 700000.0)
+   {
+    NewPay = setCommission(1.2) * setPay(80000.0);
+   }
+   else
+   {
+    NewPay = setPay(80000.0);
+   }
   }
 }
 
 class callers extends Employee {
-  void getData3()
-  {
-   setPay(25000.0);
-   setLevel("Caller");
-   TComBased();
-   setValueSold(50000.0);
-   Payday = setPay(25000.0);
-   setSurveyRating(3.5);
-   setHoursWorked(45.0);
+ double Val;
+ double NewPay;
 
-   task();
-
-   if (Serv > 4.4)
-   {
-    double Com = setCommission(1.5);
-    NewPay = Com * Payday;
-   }
-   else if (Serv > 3.9)
-   {
-    Com = setCommission(1.3);
-    NewPay = Com * Payday;
-   }
-   else if (Serv > 2.9)
-   {
-    Com = setCommission(1.1);
-    NewPay = Com * Payday;
-   }
-   else
-   {
-    getCommission();
-    NewPay = Payday;
-   }
-  }
-}
-
-class cusService extends Employee {
- void getData4()
- {
-  setLevel("Customer Service");
-  getComBased();
-  getHoursWorked();
-  Payday = setPay(30000.0);
-  Serv = setSurveyRating(4.5);
-  if (Serv > 4.4)
+ void getData3() {
+  setLevel("Caller");
+  TComBased();
+  Val = setValueSold(50000.0);
+  setPay(25000.0);
+  setSurveyRating(3.5);
+  setHoursWorked(45.0);
+  task();
+  if (Val > 100000.0)
   {
-   Com = setCommission(1.5);
-   NewPay = Com * Payday;
+   NewPay = setCommission(2.5) * setPay(25000.0);
   }
-  else if (Serv > 3.9)
+  else if (Val > 75000.0)
   {
-   Com = setCommission(1.3);
-   NewPay = Com * Payday;
+   NewPay = setCommission(2.0) * setPay(25000.0);
   }
-  else if (Serv > 2.9)
+  else if (Val > 50000.0)
   {
-   Com = setCommission(1.1);
-   NewPay = Com * Payday;
+   NewPay = setCommission(1.5) * setPay(25000.0);
   }
   else
-  {
-   getCommission();
-   NewPay = Payday;
+   {
+   NewPay = setPay(25000.0);
   }
  }
 }
 
-class janitors extends Employee {
-  void getData5()
-  {
+
+ class cusService extends Employee {
+  double Serv;
+  double NewPay;
+
+  void getData4() {
+   setLevel("Customer Service");
+   TComBased();
+   getHoursWorked();
+   Serv = setSurveyRating(4.5);
+   if (Serv > 4.4)
+   {
+    NewPay = setCommission(1.5) * setPay(35000.0);
+   } else if (Serv > 3.9)
+   {
+    NewPay = setCommission(1.3) * setPay(35000.0);
+   }
+   else if (Serv > 2.9)
+   {
+    NewPay = setCommission(1.1) * setPay(35000.0);
+   }
+   else
+    {
+    NewPay = setPay(35000.0);
+   }
+  }
+ }
+
+ class janitors extends Employee {
+  void getData5() {
    setLevel("Janitor");
-   setComBased("No");
+   FComBased();
+   setPay(25000.0);
+   setHoursWorked(25.0);
    task();
   }
-}
+ }
