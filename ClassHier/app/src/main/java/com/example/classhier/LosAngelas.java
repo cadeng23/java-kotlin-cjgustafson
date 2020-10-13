9335d9d0d6bb010d0c76a9b1ffb4b60e1fe235d3
Caden Gustafson <gustafsoncaden@gmail.com>
2020-10-13 15:07:22 -0600

    Beginning list examples

---
+++ ClassHier/app/src/main/java/com/example/classhier/LosAngelas.java
@@ -0,0 +1,17 @@
+package com.example.classhier;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import static java.lang.Math.*;
+
+public class LosAngelas implements Comparable < LosAngelas > {
+    public double getLatitude() {
+        return latitude;
+    }
+    @Override
+    public int compareTo(LosAngelas losAngelas) {
+        return 0;
+    }
+}
---
+++ ClassHier/app/src/main/java/com/example/classhier/TeleCorp.java
@@ -0,0 +1,17 @@
+package com.example.classhier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class TeleCorp {
+
+}
